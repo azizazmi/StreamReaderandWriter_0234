4cf459b81fe115420c567f95592cc30fae7d3b4a
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-06-04 14:49:09 +0700

    menambahkan perintah untuk membuka contohfile.txt pada object infile

--- ioFile.cpp
+++ ioFile.cpp
@@ -28,6 +28,8 @@ int main() {
 
     //Membuka file dalam mode membaca
     ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
 
     cout << endl << ">= Membuka dan membaca file " << endl;
 
